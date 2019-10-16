4b732b28b4178f5b2c88b32cad909d68d2504710
Tiago Manuel Longras Araujo <tiagoxxp@gmail.com>
2019-10-16 13:01:20 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 chmod a+x ./deploy.sh
 
-export FILE_PATH="$(pwd)/$PROJECT/installer/$ARCH_FOLDER"
+export FILE_PATH="$(pwd)/"
 export SOFTWARE_VERSION="$(sed -n -e '/VERSION =/ s/.*\  *//p' displax-connect/QtInstallerFramework/Installer.pro)"
 
 npm --prefix deploy-script/ install deploy-script/
